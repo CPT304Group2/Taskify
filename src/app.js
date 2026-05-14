bf30e1f8efcab0289df012a1f17d270415bbab32
Bobowen-Shi <Bowen.Shi22@student.xjtlu.edu.cn>
2026-05-14 18:50:18 +0800

    feat: add Cookie banner and Privacy Policy page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const { urlencoded } = require("express");
 const express = require("express");
 const path = require("path");
+const cookieParser = require('cookie-parser');
 require("dotenv").config();
 require("../src/db/conn");
 const views_path = path.join(__dirname, "../views");
@@ -11,11 +12,17 @@ const port = process.env.PORT || 80;
 
 app.use("/static", express.static(static_path));
 app.use(express.json());
+app.use(cookieParser());
 app.use(urlencoded({ extended: false }));
 
 
 app.set("view engine", "ejs");
 app.set("views", views_path);
+// Cookie consent middleware
+app.use((req, res, next) => {
+    res.locals.cookiesAccepted = req.cookies.cookies_accepted === 'true';
+    next();
+});
 
 app.get("/", (req, res) => {
     res.status(200).render("index.ejs");
@@ -28,6 +35,10 @@ app.get("/signup", (req, res) => {
 // In Future this dashboard will be rendered after authentication of users 
 app.get("/dashboard", (req, res) => {
     res.status(200).render("dashboard/dashboard.ejs");
+});
+// Privacy Policy page
+app.get("/privacy", (req, res) => {
+    res.render("privacy");
 });
 
 
